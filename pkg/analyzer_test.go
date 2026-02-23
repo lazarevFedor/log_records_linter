63c39fc20c4c790faf1cded1ef05d500a9d3aab8
lazarevFedor <lazarevfedor3@gmail.com>
2026-02-23 07:59:12 +0300

    add: implement test for LogsAnalyzer using analysistest package

--- pkg/analyzer_test.go
+++ pkg/analyzer_test.go
@@ -1,1 +1,12 @@
 package pkg
+
+import (
+	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func TestAll(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, LogsAnalyzer, "")
+}
